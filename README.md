bc7b5747e8658c2ecb7448c67ad325c4ef77f1c7
Carl-Erik Kopseng <carlerik@gmail.com>
2019-08-05 15:37:57 +0200

    First working concept

---
+++ README.md
@@ -0,0 +1,38 @@
+# stacktrace-cli
+> Retroactively show a source mapped location from a error message
+
+## Example:
+You have such an error in your logs: 
+```
+https://localhost:56962/app-bundle.js 129:600036 TypeError: Cannot read property 'map' of undefined
+```
+
+If you download then download `app-bundle.js` and `app-bundle.js.map` you can get the original 
+source lines using this tool:
+
+```
+ npx stacktrace-cli dist/app-bundle.js* 129:600036
+{
+    "columnNumber": 56,
+    "lineNumber": 78,
+    "fileName": "webpack:///src/store/entitys/entitys-reducer.js",
+    "functionName": "getEntitysById"
+}
+{
+    "columnNumber": 56,
+    "lineNumber": 78,
+    "fileName": "webpack:///src/store/entitys/entitys-reducer.js",
+    "functionName": "map"
+}
+```
+
+## How?
+- Stacktrace.js
+- Node HTTP
+- Puppeteer
+
+Stacktrace.js is wonderful. In theory. When trying to use it, the docs are 
+missing all the bits on how to get this running. This is my attempt at 
+solving [an itch that needed scratching](https://stackoverflow.com/questions/57346131/convert-columnerror-to-original-line-numbers-using-a-source-map).
+
+This was literally hacked together in 3 hours, so feel free to extract, refactor and package it up as the sweet little thing it could have been.
---
+++ stack-cli.js
@@ -0,0 +1,132 @@
+#!/usr/bin/env node
+/* eslint-disable no-process-exit */
+
+const usageStr = `
+USAGE: stack-cli your-bundle.js your-bundle.js.map LINE:COLUMN
+`;
+
+function usage() {
+  process.stdout.write(usageStr);
+  process.exit(1);
+}
+
+if (process.argv.length != 5) {
+  usage();
+}
+
+const puppeteer = require("puppeteer");
+const http = require("http");
+const fs = require("fs");
+const port = Number(process.env.PORT || 8000);
+const stackutilsName = "stackutils.js";
+const minifiedSource = process.argv[2];
+const sourceMap = process.argv[3];
+const errLocation = process.argv[4].split(":");
+const errLine = errLocation[0];
+const errColumn = errLocation[1];
+
+const htmlWithModuleScript = `
+<!-- source order matters -->
+<script src="stackutils/source-map.js"></script>
+<script src="stackutils/stackframe.js"></script>
+<script src="stackutils/stacktrace-gps.js"></script>
+<script src="stackutils/error-stack-parser.js"></script>
+<script src="stackutils/stacktrace.js"></script>
+
+<script>
+async function main(){
+    var stackframe = new StackFrame({fileName: 'http://localhost:${port}/${minifiedSource}', lineNumber: ${errLine}, columnNumber: ${errColumn}});
+
+    var callback = function myCallback(stackframe) { console.log(JSON.stringify(stackframe, null, 4)); };
+
+    var errback = function myErrback(error) { 
+                console.log('stacktrace utils error: ', error.message); 
+                //StackTrace.fromError(error).then(enhancedErr => console.log('enhanced', enhancedErr)); 
+    };
+
+    var gps = new StackTraceGPS();
+
+    // Pinpoint actual function name and source-mapped location
+    await gps.pinpoint(stackframe).then(callback, errback);
+
+    // Better location/name information from source maps
+    await gps.getMappedLocation(stackframe).then(callback, errback);
+
+    // For some reason, this doesn't complete in Puppeteer, but does in normal Chrome 
+    // Get function name from location information
+    //await gps.findFunctionName(stackframe).then(callback, errback);
+    
+    console.log("finito")
+}
+
+main();
+</script>
+`;
+
+async function evaluatePageContent() {
+  const browser = await puppeteer.launch({
+    // https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md#running-puppeteer-on-travis-ci
+    args: ["--no-sandbox"],
+    // allow overriding chrome path
+    executablePath: process.env.SINON_CHROME_BIN || null
+  });
+  const page = await browser.newPage();
+
+  function die(reason) {
+    if (reason) {
+      /* eslint-disable no-console */
+      console.error(reason);
+    }
+
+    browser.close();
+    process.exit(1);
+  }
+
+  page.on("error", function(err) {
+    throw err;
+  });
+
+  // our "assertion framework" :)
+  page.on("console", function(msg) {
+    var text = msg.text();
+
+    if (text === "finito") {
+      browser.close();
+      process.exit(0);
+    }
+
+    console.log(text);
+  });
+
+  await page.goto(`http://localhost:${port}`);
+
+  setTimeout(() => die("No result within timeout."), 1000);
+}
+
+const app = http.createServer((req, res) => {
+  let body, type;
+
+  const match = req.url.match(/stackutils\/(.*)/);
+  if (match) {
+    body = fs.readFileSync(`${__dirname}/assets/${match[1]}`);
+    type = "application/javascript";
+  } else if (req.url.match(new RegExp(minifiedSource + "$"))) {
+    body = fs.readFileSync(minifiedSource);
+    type = "application/javascript";
+  } else if (req.url.match(new RegExp(sourceMap + "$"))) {
+    body = fs.readFileSync(sourceMap);
+    type = "application/octet-stream";
+  } else {
+    body = htmlWithModuleScript;
+    type = "text/html";
+  }
+
+  const headers = {
+    "Content-Length": Buffer.byteLength(body),
+    "Content-Type": type
+  };
+  res.writeHead(200, headers);
+  res.end(body);
+});
+
+app.listen(port, evaluatePageContent);
