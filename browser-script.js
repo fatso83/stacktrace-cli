2fe7fde7950c5882d59386c80ce090573876fa46
Carl-Erik Kopseng <carlerik@gmail.com>
2019-08-09 17:10:50 +0200

    Move logic into separate scripts
    PORT=8001 stacktrace-cli ~/Downloads/1.bundle.js ~/Downloads/1.bundle.js.map 129:599695

---
+++ browser-script.js
@@ -0,0 +1,30 @@
+async function main(port, lineNumber, columnNumber) {
+  var stackframe = new StackFrame({
+    fileName: `http://localhost:${port}/bundle.js`,
+    lineNumber,
+    columnNumber
+  });
+
+  var callback = function myCallback(stackframe) {
+    console.log(JSON.stringify(stackframe, null, 4));
+  };
+
+  var errback = function myErrback(error) {
+    console.log("stacktrace utils error: ", error.message);
+    //StackTrace.fromError(error).then(enhancedErr => console.log('enhanced', enhancedErr));
+  };
+
+  var gps = new StackTraceGPS();
+
+  // Pinpoint actual function name and source-mapped location
+  await gps.pinpoint(stackframe).then(callback, errback);
+
+  // Better location/name information from source maps
+  await gps.getMappedLocation(stackframe).then(callback, errback);
+
+  // For some reason, this doesn't complete in Puppeteer, but does in normal Chrome
+  // Get function name from location information
+  //await gps.findFunctionName(stackframe).then(callback, errback);
+
+  console.log("finito");
+}
